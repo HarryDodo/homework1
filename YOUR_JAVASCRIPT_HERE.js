c49795e8c40a4861fb23768b2339f2623c67fde6
“HarryDodo” <harrydijkema@gmail.com>
2018-10-21 14:41:52 +0200

    denk dat het zo moet

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,15 @@
-// Write your JS here
+const hero = {
+    name: "Harry Dijkema",
+    heroic: true,
+    inventory: [1, 2],
+    health: 10,
+    weapon: {
+        type: "bazooka",
+        damage: 2
+    }
+}
+
+function rest(hero) {hero.health = 10
+return hero}
+function pickUpItem() {}
+function equipWeapon() {}
